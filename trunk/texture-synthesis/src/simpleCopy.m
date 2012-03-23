96bbda136484b484e2202f4a8be3408a6c9a8be9
kirai.chang <kirai.chang@40e1e216-378d-185b-cb53-7c1952ec5148>
2012-03-23 09:06:58 +0000

    modify copy

--- trunk/texture-synthesis/src/simpleCopy.m
+++ trunk/texture-synthesis/src/simpleCopy.m
@@ -1,11 +1,33 @@
-function image = simpleCopy( fileName, originPos, size)
+function image = simpleCopy( fileName, originPos, winSize, multi)
 %simpleCopy Summary of this function goes here
 %   simpleCopy is a function to copy a image with fileName
 %   start with originPos
 %   end at size
 %   and copy power.
-    origin = imread(fileName);
-    doubleOrigin = im2Double(origin);
-    image = [doubleOrigin(originPos(1): size(1), originPos(2): size(2),:), doubleOrigin(originPos(1): size(1), originPos(2): size(2),:); doubleOrigin(originPos(1): size(1), originPos(2): size(2),:), doubleOrigin(originPos(1): size(1), originPos(2): size(2),:)];
+    origin = im2Double(imread(fileName));
+    oriSize = size(origin);
+    
+    %check window not out of bound
+    for i = 1: 2
+        if winSize(i) > oriSize(i)
+            winSize(i) = oriSize(i);
+        end
+    end
+    image = [origin(originPos(1): winSize(1), originPos(2): winSize(2),:), origin(originPos(1): winSize(1), originPos(2): winSize(2),:);
+        origin(originPos(1): winSize(1), originPos(2): winSize(2),:), origin(originPos(1): winSize(1), originPos(2): winSize(2),:)];
+    
+    imageSize = size(image);
+    
+    % process x side
+    while imageSize(1) + winSize(1) <= oriSize(1) * multi
+        image = [image(:,:,:); image(originPos(1):winSize(1),:,:)];
+        imageSize = size(image);
+    end
+    if imageSize(1) < oriSize(1) * multi
+        image = [image(:,:,:), image(originPos(1):oriSize(1) * multi - imageSize(1),:,:)];
+    end
+    
+    % process y side
+    %image = [image(:,:,:);image(:,:,:)];
 end
 
