f2af1f6139d4283dda1cbc4f0e1e22c131339dd5
kirai.chang <kirai.chang@40e1e216-378d-185b-cb53-7c1952ec5148>
2012-05-25 09:53:20 +0000

    modify EE

--- trunk/image_abstraction/example2.m
+++ trunk/image_abstraction/example2.m
@@ -2,9 +2,15 @@ F_nc = imread('DoGSimple.png');
 F_ncd = im2double(F_nc);
 
 F_eed = EE(F_ncd);
-%F_ee = double2im(F_eed);
-%figure, imshow(F_ee); title('Edge Enhancement');
-%    for i = 1:3
-%        F_eec(1:size(F_nc, 1), 1:size(F_nc, 2), i) = F_nc(1:size(F_nc, 1), 1:size(F_nc, 2), i) - F_ee;
-%    end
-%figure, imshow(F_eec); title('Image Combine');
+figure, imshow(F_eed); title('Edge Enhancement');
+F_eec = zeros(size(F_ncd));
+for x = 1:size(F_eed, 1)
+    for y = 1:size(F_eed, 2)
+        if F_eed(x, y) == 0
+            F_eec(x, y, 1:3) = 0;
+        else
+            F_eec(x, y, 1:3) = F_ncd(x, y, 1:3);
+        end
+    end
+end
+figure, imshow(F_eec); title('Image Combine');
