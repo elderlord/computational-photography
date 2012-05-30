f482f9cdd1c3fbb75a3a24bcc1a1a4e2e86105ff
kirai.chang <kirai.chang@40e1e216-378d-185b-cb53-7c1952ec5148>
2012-05-30 09:12:22 +0000

    add DoG into the project

--- trunk/image_abstraction/CM.m
+++ trunk/image_abstraction/CM.m
@@ -3,8 +3,8 @@ function [ result ] = CM( image_origin, image_ee )
     result = zeros(size(image_origin));
     for x = 1:size(image_ee, 1)
         for y = 1:size(image_ee, 2)
-            if image_ee(x, y) == 0
-                result(x, y, 1:3) = 0;
+            if image_ee(x, y) < 1
+                result(x, y, 1:3) = [image_ee(x,y), image_ee(x,y),image_ee(x,y)];
             else
                 result(x, y, 1:3) = image_origin(x, y, 1:3);
             end
