001308475b09ba1ec36a11e5064875db2d053fd6
kirai.chang <kirai.chang@40e1e216-378d-185b-cb53-7c1952ec5148>
2012-05-31 08:52:43 +0000

    modify bug

--- trunk/image_abstraction/DoG.m
+++ trunk/image_abstraction/DoG.m
@@ -33,15 +33,18 @@ function result = DoG( img, sigma_e, sigma_r, tau, phi)
 
             for i = -halfWidth:halfWidth%    for ( int i = -halfWidth; i <= halfWidth; ++i ) {
                 for j = -halfWidth:halfWidth%        for ( int j = -halfWidth; j <= halfWidth; ++j ) {       
-                    d = sqrt(i*i)+(j*j);% float d = length(vec2(i,j));
+                    d = sqrt((i*i)+(j*j));% float d = length(vec2(i,j));
                     kernel = [exp(-d * d / twoSigmaESquared ), exp( -d * d / twoSigmaRSquared )];%vec2 kernel = vec2( exp( -d * d / twoSigmaESquared ), exp( -d * d / twoSigmaRSquared ));
                     u = abs(floor((uv(1, 1) + i)));
                     v = abs(floor((uv(1, 2) + j)));
                     if u > 0 && u < img_size(1, 1) && v > 0 &&  v < img_size(1, 2)
-                        Y = 0.299*img(u, v, 1)+0.587*img(u, v, 2)+0.114*img(u, v, 3);
+                        %Y = 0.299*img(u, v, 1)+0.587*img(u, v, 2)+0.114*img(u, v, 3);
                         %U = 0.436*(img(u, v, 3)-Y/(1-0.114));
                         %V = 0.615*(img(u, v, 1)-Y/(1-0.299));
-                        L = [Y,Y];%            vec2 L = texture2D(img, uv + vec2(i,j) / img_size).xx;
+                        X = img(u, v, 1) / 100.0;
+                        %Y = 0.5 + 0.5 * ( img(u, v, 2) / 127.0 );
+                        %Z = 0.5 + 0.5 * ( img(u, v, 3) / 127.0 );
+                        L = [X, X];%            vec2 L = texture2D(img, uv + vec2(i,j) / img_size).xx;
                         norm = norm + 2.0 * kernel;%            norm += 2.0 * kernel;
                         sum = [sum(1, 1) + kernel(1, 1) * L(1, 1), sum(1, 2) + kernel(1, 2) * L(1, 2)];%            sum += kernel * L;
                     end
